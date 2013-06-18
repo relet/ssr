bb5522e0d69490f35d60160a51a5249dcae36b5a
Thomas Hirsch <thomas.hirsch@statkart.no>
2013-06-18 14:43:10 +0200

    switch to osmapi

--- scripts/manual.py
+++ scripts/manual.py
@@ -11,12 +11,18 @@ import simplejson as json
 import sys
 import requests
 from   codecs import        open, getreader
-#try:
-from   lxml import etree as tree
-#except:
-#  import xml.etree.cElementTree as tree
+try:
+  from   lxml import etree as tree
+except:
+  import xml.etree.cElementTree as tree
 from   urllib import        urlopen, quote_plus 
 from   math import          sqrt
+try:
+  from Levenshtein import ratio
+except:
+  pass
+import OsmApi
+
 
 #XAPI_URL="http://jxapi.osm.rambler.ru/xapi/api/0.6/*[bbox=%s,%s,%s,%s]"
 XAPI_URL="http://www.overpass-api.de/api/xapi?*[bbox=%s,%s,%s,%s]"
@@ -28,21 +34,35 @@ EDIT_URL = "http://www.openstreetmap.org/edit?editor=id&lat=%.4f&lon=%.4f&zoom=1
 NK_URL   = "http://beta.norgeskart.no/?sok=%.4f,%.4f#14/%.4f/%.4f"
 NODE_URL = "http://www.openstreetmap.org/?node=%s" 
 
-API    = "http://api.openstreetmap.org"
-
 navntype    = json.loads(open("navntype.json","r","utf-8").read())
 credentials = json.loads(open("credentials.json","r","utf-8").read())
+username, password = credentials
+
+api = OsmApi.OsmApi(username=username, password=password, api="api.openstreetmap.org")
 
 osmtypes    = {
   6:  ("node", "natural=peak"),   # kollen - massif?
+  8:  ("node", "natural=peak"),   # haug - mound?
+  10: ("node", "natural=hillside"), # li - imaginary tag
   16: ("way" , "natural=valley"), # dal - only way and area!
+  17: ("node" , "natural=valley"), # botn - the end of a valley
   31: ("area", "natural=water"),  # vann
+  32: ("area", "natural=water"),  # tjern
+  35: ("node", "natural=bay"),    # vik
+  36: ("way" , "waterway=river"), # elv
   37: ("way" , "waterway=river"), # bekk
-  83: ("node", "natural=bay"),    # vik
-  87: ("node", "natural=cape"),   # nes
+  47: ("node", "natural=cape"),   # nes
+  61: ("area", "natural=wetland"),   # myr + wetland=marsh
+  84: ("node", "natural=strait"), # sund
+  85: ("area", "place=island"),   # holme
+  83: ("node", "natural=bay"),    # vik i sjø
+  87: ("node", "natural=cape"),   # nes i sjø
   104:("node", "place=farm"),     # grend
   108:("node", "place=farm"),     # bruk
+  109:("area", "building=house"), # enebolig
   110:("area", "building=cabin"), # fritidsbolig, area!
+  216:("relation", "place=island"), # øppe
+  218:("node", "landuse=quarry"), # grustak/steinbrudd
 }
 
 skrstat = {
@@ -56,9 +76,10 @@ tystat = {
 langcode = {
   "NO": "no",
   "SN": "se",
+  "SS": "sma",
 }
 
-self, fin, fout = sys.argv
+self, fin = sys.argv
 
 data = geojson.loads(open(fin,"r","utf-8").read())
 features = data['features']
@@ -71,22 +92,21 @@ def identify(feature):
   forname  = feature['properties']['for_snavn']
 
   if sname != forname:
-    print sname,"!=",forname,"!!!111", "avslått?",  skrstat[feature['properties']['skr_snskrstat']]
-    return
+    return #usually avslatt
 
   ssrid    = feature['properties']['enh_ssr_id']
 
   XTOL = TOL * 100
   queryByName = (XAPI_URL + NAME_Q) % (lon-XTOL, lat-XTOL, lon+XTOL, lat+XTOL, quote_plus(sname.encode('utf-8')))
   queryWoName = (XAPI_URL) % (lon-TOL, lat-TOL, lon+TOL, lat+TOL)
-  osm = tree.fromstring(requests.get(queryWoName).text)
-  #names = osm.findall(".//tag")
-  names = osm.findall(".//tag[@k='name']")
+  osm = tree.fromstring(requests.get(queryWoName).text.encode('utf-8'))
+  names = osm.findall(".//tag")
+  #names = osm.findall(".//tag[@k='name']")
     
   if len(names) == 0:
-    osm = tree.fromstring(requests.get(queryByName).text)
-    #names = osm.findall(".//tag")
-    names = osm.findall(".//tag[@k='name']")
+    osm = tree.fromstring(requests.get(queryByName).text.encode('utf-8'))
+    names = osm.findall(".//tag")
+    #names = osm.findall(".//tag[@k='name']")
 
   status[ssrid] = {}
   status[ssrid]['found']=False
@@ -95,10 +115,17 @@ def identify(feature):
   for name in names:
     if not name.get('k') == 'name':
       continue
+    try: 
+      parent = name.getparent() #lxml
+    except:
+      parent_map = dict((c, p) for p in osm.getiterator() for c in p)
+      parent = parent_map[name] 
     osmname = unicode(name.get('v'))
-    osmid   = name.getparent().get('id')
-    osmlon  = name.getparent().get('lon') # only works for nodes, or we'll a) have to fetch references b) find a better distance calculation
-    osmlat  = name.getparent().get('lat')
+    osmid   = parent.get('id')
+    osmlon  = parent.get('lon') # only works for nodes, or we'll a) have to fetch references b) find a better distance calculation
+    osmlat  = parent.get('lat')
+    #TODO: extract all "tag" elements from parent
+    
     if osmlon:
       dx = float(osmlon)-lon
       dy = float(osmlat)-lat
@@ -112,15 +139,22 @@ def identify(feature):
         status[ssrid]['found']=True
         status[ssrid]['nodes']=[{"osmid":osmid, "distance":distance}]
         print "IDENTIFIED", osmname
-        source = name.getparent().get('source')
-        link   = name.getparent().get('source_id')
-        if source == "Kartverket" and link:
-          print "linked to ssr"
-        else:
-          print "not linked to ssr. TODO: display edit/update options"
-          print "ID Editor: ", EDIT_URL % (osmlat,osmlon)
+
+        #TODO: compare source and source_id tags 
+        #if source == "Kartverket" and link:
+        #  print "linked to ssr"
+        #  if link == ssrid:
+        #    print "CORRECT!"
+        #  else:
+        #    print link, ssrid
+        #else:
+        #  print "not linked to ssr."# TODO: display edit/update options
+        #  print "ID Editor: ", EDIT_URL % (float(osmlat),float(osmlon))
     else:
-      delta = max( ratio(osmname, sname), ratio(osmname, forname) )
+      try:
+        delta = max( ratio(osmname, sname), ratio(osmname, forname) ) # Levenshtein may not be available
+      except:
+        delta = 1
       if delta > bestratio:
         status[ssrid]['bestmatch'] = {"osmname":osmname, "osmid":osmid, "levenshtein":delta}
         bestratio = delta
@@ -143,7 +177,6 @@ def identify(feature):
     # - edit an existing area, way or node
 
     geomtype, osmtype = osmtypes.get(int(typ), (None,None))
-    username, password = credentials
 
     print
     print "CHOOSE WISELY:"
@@ -159,63 +192,44 @@ def identify(feature):
     if userin in ["x\n", "X\n"]:
       sys.exit(0)
     elif userin in ["a\n", "A\n"]:
-      r = requests.get(API + "/api/0.6/permissions", auth = (username, password))   
-      perms  = tree.fromstring(r.text.encode('utf-8'),tree.XMLParser(encoding='utf-8'))
-      if not perms.findall(".//permission[@name='allow_write_api']"):
+      #r = requests.get(API + "/api/0.6/permissions", auth = (username, password))   
+      #perms  = tree.fromstring(r.text)#.encode('utf-8'),tree.XMLParser(encoding='utf-8'))
+      ##if not perms.findall(".//permission[@name='allow_write_api']"):
       #if not perms.findall(".//permission"):
-        print "Permission denied."
-        print tree.tostring(perms)
-        sys.exit(1)
- 
+      #  print "Permission denied."
+      #  print tree.tostring(perms)
+      #  sys.exit(1)
 
       typekey, typeval = osmtype.split("=")
 
       comment = "Adding single element from the central place name register of Norway (SSR): %s" % sname
+      csid    = api.ChangesetCreate({
+          "comment":comment,
+          "created_by":"ssr-api alpha",
+      })
 
-      changeset = """
-       <osm>
-         <changeset>
-           <tag k="created_by" v="ssr-api alpha"/>
-           <tag k="comment" v="%s"/>
-         </changeset>
-       </osm>
-      """ % (comment,)
-
-      #create changeset
-      r = requests.put(API + "/api/0.6/changeset/create", auth = (username, password), data = changeset)   
-
-      csid = r.text
       print "Creating changeset id #", csid
 
-      element = tree.fromstring("""
-        <osm>
-          <node changeset="%s" lat="%.6f" lon="%.6f">
-            <tag k="name" v="%s"/>
-            <tag k="name:%s" v="%s"/>
-            <tag k="official_name" v="%s"/>
-            <tag k="source" v="Kartverket"/>
-            <tag k="source_id" v="%s"/>
-            <tag k="source_ref" v="http://faktaark.statkart.no/SSRFakta/faktaarkfraobjektid?enhet=%s"/>
-            <tag k="%s" v="%s"/> 
-          </node>
-        </osm>
-      """ % (csid, lat, lon, sname, lang, sname, sname, ssrid, ssrid, typekey, typeval)
-      )
+      if geomtype == "node":
+        node = api.NodeCreate({
+          "lat":"%.6f"%lat,
+          "lon":"%.6f"%lon,
+          "tag":{ 
+            "name":sname,
+            "name:%s" % lang: sname,
+            "official_name":sname,
+            "source":"Kartverket",
+            "source_id":unicode(ssrid),
+            "source_ref":"http://faktaark.statkart.no/SSRFakta/faktaarkfraobjektid?enhet=%s" % ssrid,
+            typekey: typeval,
+        }})
+
+        print "Created element", NODE_URL % node["id"]
+
+      api.ChangesetClose()
 
 
-      #print tree.tostring(element)
-      r = requests.put(API + "/api/0.6/node/create", auth = (username, password), data = tree.tostring(element))   
-
-      print r 
-      oid = r.text
-      
-      print "Created element", NODE_URL % oid
-      if geomtype != "node":
-        print "Please EDIT IMMEDIATELY: ", EDIT_URL % (lat, lon)
-      
-      r = requests.put(API + "/api/0.6/changeset/#%s/close" % csid, auth = (username, password))   
-
-map(identify, features[8:20])
+map(identify, features[:20])
 
 #fd = open(fout, "w")
 #fd.write(json.dumps(status))
